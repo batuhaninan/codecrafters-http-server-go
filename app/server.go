0d39ee66dfc775f929310ded2fd83cb67a1c00c7
Batuhan Inan <Batuhan.Inan@softtech.com.tr>
2024-08-10 19:39:33 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,22 +2,57 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"net"
 	"os"
 )
 
+type ServerOpts struct {
+	Port int
+}
+
+type HttpOpts struct {
+	Version string
+}
+
+type Server struct {
+	Opts     ServerOpts
+	HttpOpts HttpOpts
+}
+
+type HttpStatus struct {
+	StatusCode int
+	Reason     string
+}
+
+var OK = HttpStatus{
+	StatusCode: 200,
+	Reason:     "OK",
+}
+
 func main() {
 	fmt.Println("Server started")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	server := Server{
+		Opts: ServerOpts{
+			4221,
+		},
+		HttpOpts: HttpOpts{
+			Version: "1.1",
+		},
+	}
+
+	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", server.Opts.Port))
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		slog.Error("Failed to bind to port:", "port", server.Opts.Port)
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		slog.Error("Error accepting connection:", "error", err.Error())
 		os.Exit(1)
 	}
+
+	conn.Write([]byte(fmt.Sprintf("HTTP/%s %d %s\r\n\r\n", server.HttpOpts.Version, OK.StatusCode, OK.Reason)))
 }
